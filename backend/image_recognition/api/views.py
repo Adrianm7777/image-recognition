fbb4795eb84715845d25a772287038b71d0c1e37
Adrianm7777 <Adrianmaterka10@gmail.com>
2024-10-16 13:46:43 +0200

    feature: Added view to handle image upload

--- backend/image_recognition/api/views.py
+++ backend/image_recognition/api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.parsers import MultiPartParser, FormParser
 
-# Create your views here.
+class ImageUploadView(APIView):
+    parser_classes = [MultiPartParser, FormParser]
+
+    def post(self, request, *args, **kwargs):
+        image = request.data.get('file')
+
+        if not image:
+            return Response({'error': 'No image provided'}, status=400)
+        
+        return Response({'message': 'Image received'}, status=200)
